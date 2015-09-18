bf60fc20c2a2f976711966f59129a6a68e40979b
Paula Origlio <minplynn@aol.com>
2015-09-18 10:41:27 -0600

    finished, only needs style and fine-tuning

--- script.js
+++ script.js
@@ -2,6 +2,26 @@ angular.module("formModule",[])
 
 angular.module("formModule").controller("formController",["$scope", function($scope){
 
-	
+	$scope.showButtonClick = function ( $event ) {
+		$scope.visibility = true;
+		$scope.editUserName = ""
+		$scope.editUserInfo = ["","",""]
+	}
+
+	$scope.hideButtonClick = function ( $event ) {	
+		$scope.visibility = false;
+	}
+
+	$scope.userName = "Name"
+	$scope.userInfo = ["Species: ","Location: ","Profession: "]
+
+	$scope.submit = function ( $event ) {
+		$scope.userName = $scope.editUserName
+		$scope.visibility = false; 
+		$scope.userInfo = $scope.editUserInfo
+	}
+
+	$scope.editUserName = ""
+	$scope.editUserInfo = ["","",""]
 
 }])
